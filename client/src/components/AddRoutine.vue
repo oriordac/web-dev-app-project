e47cd2c294ccf470dc2796732fe3ab38e3b9ae65
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-03-25 21:13:39 -0400

    Convert add new routine modal into component

---
+++ client/src/components/AddRoutine.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="modal" :class="{'is-active': isForm}">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <div class="box">
+            
+            <div class="field">
+            <label class="label">Title</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Title" v-model="newTitle">
+            </div>
+            <p class="help">Enter Rountine Title</p>
+            </div>
+
+            <div class="field">
+            <label class="label">Photo URL</label>
+            <div class="control">
+                <input class="input" type="text" v-model="newPhoto">
+            </div>
+            <p class="help">Enter the web address of an img/png file</p>
+            </div>
+
+            <div class="field">
+            <label class="label">Excerise 1</label>
+            <div class="control">
+                <input class="input" type="text" v-model="newExcer1">
+            </div>
+            <p class="help">Enter the name of the excercise</p>
+            </div>
+
+            <div class="field">
+            <label class="label">Excerise 2</label>
+            <div class="control">
+                <input class="input" type="text" v-model="newExcer2">
+            </div>
+            <p class="help">Enter the name of the excercise</p>
+            </div>
+
+            <div class="field">
+            <label class="label">Excerise 3</label>
+            <div class="control">
+                <input class="input" type="text" v-model="newExcer3">
+            </div>
+            <p class="help">Enter the name of the excercise</p>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-info" @click="add();adminModal()">Submit</button>
+                </div>
+                <div class="control">
+                    <button class="button is-link is-light" @click="clear">Cancel</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="adminModal"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["isForm"],
+    data: () => ({
+        //v-model elements for admin form to add routines
+        newTitle: "",
+        newPhoto: "",
+        newExcer1: "",
+        newExcer2: "",
+        newExcer3: ""
+    }),
+    methods: {
+        clear() {
+            this.newTitle = "",
+            this.newPhoto = "",
+            this.newExcer1 = "",
+            this.newExcer2 = "",
+            this.newExcer3 = ""
+        },
+        add() {
+        this.$emit('add-routine', this.newTitle, this.newPhoto, this.newExcer1, this.newExcer2, this.newExcer3);
+        },
+        adminModal() {
+            this.$emit('close-admin');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/Routine.vue
+++ client/src/views/Routine.vue
@@ -77,64 +77,7 @@
     </section>
 
     <!--Modal for the adminCard only. Form to push new routine card-->
-    <div class="modal" :class="{'is-active': isForm}">
-    <div class="modal-background"></div>
-    <div class="modal-content">
-        <div class="box">
-            
-            <div class="field">
-            <label class="label">Title</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Title" v-model="newTitle">
-            </div>
-            <p class="help">Enter Rountine Title</p>
-            </div>
-
-            <div class="field">
-            <label class="label">Photo URL</label>
-            <div class="control">
-                <input class="input" type="text" v-model="newPhoto">
-            </div>
-            <p class="help">Enter the web address of an img/png file</p>
-            </div>
-
-            <div class="field">
-            <label class="label">Excerise 1</label>
-            <div class="control">
-                <input class="input" type="text" v-model="newExcer1">
-            </div>
-            <p class="help">Enter the name of the excercise</p>
-            </div>
-
-            <div class="field">
-            <label class="label">Excerise 2</label>
-            <div class="control">
-                <input class="input" type="text" v-model="newExcer2">
-            </div>
-            <p class="help">Enter the name of the excercise</p>
-            </div>
-
-            <div class="field">
-            <label class="label">Excerise 3</label>
-            <div class="control">
-                <input class="input" type="text" v-model="newExcer3">
-            </div>
-            <p class="help">Enter the name of the excercise</p>
-            </div>
-
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-info" @click="add();adminModal()">Submit</button>
-                </div>
-                <div class="control">
-                    <button class="button is-link is-light" @click="clear">Cancel</button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <button class="modal-close is-large" aria-label="close" @click="adminModal"></button>
-    </div>
+    <AddRoutine :isForm="isForm" v-on:add-routine="add" v-on:close-admin="adminModal"></AddRoutine>
 
     <!-- Modal to get to timer. This is for all cards besides the admin/add card-->
     <div class="modal" :class="{'is-active': isLog}">
@@ -149,6 +92,7 @@
 </template>
 
 <script>
+import AddRoutine from "@/components/AddRoutine.vue";
 import Workout from "@/components/Workout.vue";
 
 export default {
@@ -197,33 +141,21 @@ export default {
         //booleans to access modals on this page
         isLog: false,
         isForm: false,
-        //v-model elements for admin form to add routines
-        newTitle: "",
-        newPhoto: "",
-        newExcer1: "",
-        newExcer2: "",
-        newExcer3: ""
+        
     }),
     methods: {
         remove(i){
           this.cards.splice(i, 1);
         },
-        add() {
+        add(newTitle, newPhoto, newExcer1, newExcer2, newExcer3) {
           this.cards.push(
               { 
-                title: this.newTitle,
-                photo: this.newPhoto,
+                title: newTitle,
+                photo: newPhoto,
                 isOpen: false,
-                exercises: [this.newExcer1, this.newExcer2, this.newExcer3]
+                exercises: [newExcer1, newExcer2, newExcer3]
               }
             )
-        },
-        clear() {
-            this.newTitle = "",
-            this.newPhoto = "",
-            this.newExcer1 = "",
-            this.newExcer2 = "",
-            this.newExcer3 = ""
         },
         adminModal () {
             this.isForm = !this.isForm
@@ -233,7 +165,8 @@ export default {
         }
     },
     components: {
-        Workout
+        Workout,
+        AddRoutine
     }
 }
 </script>
