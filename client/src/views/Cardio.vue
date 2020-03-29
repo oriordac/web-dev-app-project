5e83ec52d24f6bb0cd529de713bfc17fe124f9a0
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-03-29 15:14:30 -0400

    Delete duplicate code in the comments

--- client/src/views/Cardio.vue
+++ client/src/views/Cardio.vue
@@ -44,24 +44,6 @@
             </div>
         </div>
     </section>
-    <!-- <div class="search-box">
-        <input type="text" placeholder="Search..." v-model="query" @keypress="fetchWeather" />
-    </div>
-
-    <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="location-box">
-            <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-            <div class="date">{{ dateBuilder() }}</div>
-        </div>
-
-        <div class="weather-box">
-            <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-            <div class="weather">{{ weather.weather[0].main }}</div>
-        </div>
-    </div>
-
-    <Timer></Timer> -->
-
     </div>
 </template>
 
