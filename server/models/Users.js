78b3f5885573aa552b6be8a356eb3562dd2a8b0c
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-04-04 15:08:10 -0400

    Copy model from client side to server side

---
+++ server/models/Users.js
@@ -0,0 +1,18 @@
+const Users = [
+    {Name: "Thor Odinson", Password:"Mjolnir", Email:"odinson@asgard.com", Userid: 0},
+    {Name: "Odin Allfather", Password: "Gungnir", Email: "allfather@asgard.com", Userid: 1}
+];
+
+module.exports = {
+
+    Login(username, password) {
+    const user = Users.find(x=> x.Name == username);
+    if(!user) throw Error('User not found');
+    if(user.Password != password) throw Error('Wrong Password');
+
+    return user;
+    },
+
+    Get: userid => Users[userid]
+
+}
