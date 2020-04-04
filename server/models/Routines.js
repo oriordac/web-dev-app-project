7689dd800a7ba5f4d54a9d7b1289a73242a12652
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-04-04 16:02:43 -0400

    Copy model from client side to server side

---
+++ server/models/Routines.js
@@ -0,0 +1,61 @@
+const routines =  [
+    {
+        title: "Back",
+        photo: "http://cs.newpaltz.edu/~oriordac1/assets/back.jpg",
+        isOpen: false,
+        exercises: [ "Row", "Angle Row", "Dumbbell Row" ]
+    },
+    {
+        title: "Biceps",
+        photo: "http://cs.newpaltz.edu/~oriordac1/assets/bicep.jpg",
+        isOpen: false,
+        exercises: [ "Dumbbell Bicep Curl", "Hammer Curl" ]
+    },
+    {
+        title: "Chest",
+        photo: "http://cs.newpaltz.edu/~oriordac1/assets/chest.jpg",
+        isOpen: false,
+        exercises: [ "Barbell Bench Press", "Incline Bench Press" ]
+    },
+    {
+        title: "Shoulders",
+        photo:"http://cs.newpaltz.edu/~oriordac1/assets/shoulder.jpg",
+        isOpen: false,
+        exercises: [ "Barbell Shoulder Press", "Dumbbell Shoulder Press" ]
+    },
+    {
+        title: "Triceps",
+        photo: "http://cs.newpaltz.edu/~oriordac1/assets/tricep.jpg",
+        isOpen: false,
+        exercises: [ "Dumbbell Kickbacks", "Seated Tricep Press" ]
+    },
+    {
+        title: "Legs",
+        photo: "http://cs.newpaltz.edu/~oriordac1/assets/legs.jpg",
+        isOpen: false,
+        exercises: [ "Deadlift", "Squat", "Leg Curl" ]
+    },
+];
+
+module.exports = {
+
+    remove(i) {
+        this.routines.splice(i, 1);
+    },
+    add(newTitle, newPhoto, newExcer1, newExcer2, newExcer3) {
+        this.routines.push(
+            { 
+            title: newTitle,
+            photo: newPhoto,
+            isOpen: false,
+            exercises: [newExcer1, newExcer2, newExcer3]
+            }
+        )
+    },
+    adminModal () {
+        this.isForm = !this.isForm
+    },
+    logModal () {
+        this.isLog = !this.isLog
+    }
+}
