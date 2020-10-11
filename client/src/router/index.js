ad8a928bea74304b5cac93407dc23a37c5b13c54
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-10-11 15:07:24 -0400

    Remove unused pages from front-end view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Login from '../views/Login.vue'
-// import Signup from '../views/Signup.vue'
 import Profile from '../views/Profile.vue'
 import Routine from '../views/Routine.vue'
 
@@ -15,7 +14,6 @@ const routes = [
   {path: '/', name: 'Home', component: Home},
   {path: '/about', name: 'About', component: About},
   {path: '/login', name: 'Login', component: Login},
-  {path: '/signup', name: 'Signup', component: Signup},
   {path: '/profile', name: 'Profile', component: Profile, meta: { isSecret: true }},
   {path: '/routine', name: 'Rountine', component: Routine, meta: { isSecret: true }},
   
